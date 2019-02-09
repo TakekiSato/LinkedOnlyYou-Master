93decc4203989c7d3a9c8200dc7ab14148e404f9
TakumiTokita <r.es-n_rin_ringo@docomo.ne.jp>
2019-02-09 19:12:29 +0900

    仮ゲームシーン作成（一つだけ）

---
+++ Assets/tokita/TestManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InitModel : int
+{
+    Model_Ran,
+    Model_Yu,
+    Model_Kaoru,
+    None = 99
+}
+
+
+public class TestManager : MonoBehaviour
+{
+    [SerializeField] InitModel initModel = InitModel.None;
+    ModelLoad modelLoad;
+
+    void Start()
+    {
+        modelLoad = new ModelLoad();
+        modelLoad.Load(initModel);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
