5db092692e55da31ec2a83d8ab016524fb43bf34
TakekiSato <takeyansaxplayer@gmail.com>
2019-02-09 17:40:08 +0900

    モーションモデルの追加、Resourceのテスト

---
+++ Assets/Sato/ResourceLoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject charaModel = Instantiate(Resources.Load("ran_kyoutuu 1_honban model", typeof(GameObject))) as GameObject;
+        GameObject roomModel = Instantiate(Resources.Load("room_ran", typeof(GameObject))) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
