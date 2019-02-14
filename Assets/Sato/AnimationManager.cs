fd6fba8c3c8b6e209b7a0051f1e991ebf36e6d20
TakekiSato <takeyansaxplayer@gmail.com>
2019-02-14 14:54:43 +0900

    アニメーション再生のテスト

---
+++ Assets/Sato/AnimationManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayAnime(string animName)
+    {
+        animator.Play(animName);
+        StartCoroutine(CheckIsFinishedAnime());
+    }
+
+    private IEnumerator CheckIsFinishedAnime()
+    {
+        // Play実行直後はアニメーションが再生されていないので0.3秒待つ
+        yield return new WaitForSeconds(0.3f);
+        while(true)
+        {
+            // normalizedTimeが1以上ならチェック完了
+            if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
+            {
+                Debug.Log("Finished");
+                break;
+            }
+            // 0.1秒ごとにチェックする
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
