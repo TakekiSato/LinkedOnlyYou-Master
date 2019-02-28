a282bfe7fd95e15369cb077b58770b2ff4fde34a
TakumiTokita <bardock@outlook.jp>
2019-02-28 18:58:09 +0900

    チャットシーンの音声再生用スクリプト追加。まだ音声再生まではできてない

---
+++ Assets/Sato/Scripts/ChatVoiceManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatVoiceManager : MonoBehaviour
+{
+    public AudioClip[] voiceTable = new AudioClip[1];
+
+    const int VOICE_MAX = 11;
+
+    AudioSource[] audioSources = new AudioSource[VOICE_MAX];
+
+    int nowVoiceIndex = 0;
+
+    public enum VOICE_LIST
+    {
+
+    }
+
+    void Awake()
+    {
+        for (int audioIndex = 0; audioIndex < VOICE_MAX; audioIndex++)
+        {
+            audioSources[audioIndex] = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void PlayVoice(VOICE_LIST voiceID)
+    {
+        audioSources[nowVoiceIndex].PlayOneShot(voiceTable[(int)voiceID]);
+        nowVoiceIndex++;
+        if (nowVoiceIndex >= VOICE_MAX)
+        {
+            nowVoiceIndex = 0;
+        }
+    }
+}
